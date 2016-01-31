eae577eb607bcecc2b3bda05ca3c21a23aaed5bf
Dan Amador <danamadorpe@gmail.com>
2016-01-31 01:16:28 -0600

    Wii mote IS FINALLY UPDATING, checking every X seconds

--- Assets/Scripts/WiiMoteTest.cs
+++ Assets/Scripts/WiiMoteTest.cs
@@ -6,48 +6,59 @@ using WiimoteApi.Util;
 public class WiiMoteTest : MonoBehaviour {
 	private Wiimote wiimote = null;
 	private Vector2 accelerations; 
+	public long timesPerSecond;
+	private float lastUpdate;
 	// Use this for initialization
 	void Start () {
+		lastUpdate = Time.fixedTime;
 	}
 	void Awake(){
 		if (!WiimoteManager.HasWiimote()) { 
-			Debug.Log ("shit");
+			Debug.Log ("shit is asleep");
 		}
 	}
 	// Update is called once per frame
 	void Update () {
+		Debug.Log ("Ms to update" + 1/timesPerSecond);
+		Debug.Log ("LastUpdate:" + lastUpdate);
+		Debug.Log ("time now:" + Time.fixedTime);
+		if (Time.fixedTime - lastUpdate  > 1/timesPerSecond) {
+			updateWiiMote ();
+		}
+	}
+	void updateWiiMote(){
 		float angle; 
 		if ( !WiimoteManager.HasWiimote()) { 
 
 			WiimoteManager.FindWiimotes ();
+			wiimote = WiimoteManager.Wiimotes[0];
+			wiimote.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL);
 			return;
 		}
-		wiimote = WiimoteManager.Wiimotes[0];
 		wiimote.SendPlayerLED(true, false, false, true);
 		int ret;
 		do
 		{
 			ret = wiimote.ReadWiimoteData();
+			accelerations = getAccelerationVector();
+			Debug.Log("2d vector:" +accelerations);
+			Debug.Log("3d vector:" + get3dVector());
+			angle = getAngle(accelerations);
+			Debug.Log("angle:" + angle);
 			//Debug.Log(wiimote.MotionPlus.PitchSpeed);
 			if (wiimote.Button.a) {
-				accelerations = getAccelerationVector();
-				Debug.Log("2d vector:" +accelerations);
-				Debug.Log("3d vector:" + get3dVector());
-				angle = getAngle(accelerations);
-				Debug.Log("angle:" + angle);	
+
 			}
 			if(wiimote.Button.b){
 				wiimote.Accel.CalibrateAccel (0);
 
 			}
 			if(wiimote.Button.minus){
-				FinishedWithWiimotes();
+				//FinishedWithWiimotes();
 			}
-	
-
-
 		} while (ret > 0);
-		}
+		Debug.Log ("Exit ret");
+	}
 	Vector3 get3dVector(){
 			float accel_x;
 			float accel_y;
@@ -56,7 +67,7 @@ public class WiiMoteTest : MonoBehaviour {
 
 			accel_x = accValues[0];
 			accel_y = -accValues[2];
-			accel_z = -accValues[2];
+			accel_z = -accValues[1];
 		return new Vector3 (accel_x, accel_y, accel_z);
 	}
 	float getAngle(Vector2 acc){
@@ -67,14 +78,14 @@ public class WiiMoteTest : MonoBehaviour {
 	Vector2 getAccelerationVector(){
 		float accel_x;
 		float accel_y;
-						float[] accValues = wiimote.Accel.GetCalibratedAccelData();
+		float[] accValues = wiimote.Accel.GetCalibratedAccelData();
 
 		Debug.Log ("acc:" + accValues [0]);
 
 		accel_x = accValues[0];
 		accel_y = -accValues[2];
 
-		return new Vector2(accel_x, accel_y).normalized;
+		return new Vector2(accel_x, accel_y);
 	}
 	void OnApplicationQuit(){
 		FinishedWithWiimotes();
