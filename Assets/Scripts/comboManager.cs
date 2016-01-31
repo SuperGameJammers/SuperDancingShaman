79f9647023d44fb9d3d0043a7f27fc736ea4732d
Gustavo Garcia <GustavoGS_@outlook.com>
2016-01-31 15:02:12 -0600

    Revert "Added animations to main, game, state"
    This reverts commit 7ee5b3ffd83f6166ee7624ee22614de6826345d6.

--- Assets/Scripts/comboManager.cs
+++ Assets/Scripts/comboManager.cs
@@ -4,8 +4,7 @@ using UnityEngine.UI;
 
 public class comboManager: MonoBehaviour
 {
-	public Text[] comboText;				//Campo de texto
-	public Color defaultColor, wrongColor;	//Colores texto
+	public Text comboText;	//Campo de texto
 
 	void Update ()
 	{
@@ -18,41 +17,26 @@ public class comboManager: MonoBehaviour
 			if (SV.combo >= 3)
 			{
 				positionComboText (); //Posiciona el combo de manera aleatoria
-				foreach (var item in comboText)
-				{
-					item.color = defaultColor; //Colorea el texto
-					item.text = SV.combo + " Combo!"; //Muestra el combo actual
-				}
+				comboText.text = SV.combo + " Combo!"; //Muestra el combo actual
 			}
 			else
 			{
 				positionComboText (); //Posiciona el combo de manera aleatoria
-				foreach (var item in comboText)
-				{
-					item.color = defaultColor; //Colorea el texto
-					item.text = SV.combo + " Well done!"; //Muestra un mensaje
-				}
+				comboText.text = "Well done"; //Vacia el campo de texto
 			}
 		}
 		else if (Input.anyKeyDown)
 		{
 			SV.combo = 0; //Resetea el combo
 			positionComboText (); //Posiciona el combo de manera aleatoria
-			foreach (var item in comboText)
-			{
-				item.color = wrongColor; //Colorea el texto
-				item.text = " Oops!"; //Muestra un mensaje
-			}
+			comboText.text = "Oops";
 		}
 	}
 
 	void positionComboText ()
 	{
-		foreach (var item in comboText)
-		{
-			item.rectTransform.rotation = Quaternion.Euler (0, 0, Random.Range (-45, 45)); //Establece la rotacion de manera aleatoria
-			item.fontSize = Random.Range (30, 35); //Establece el tamaño de la fuente de manera aleatoria
-		}
+		comboText.rectTransform.rotation = Quaternion.Euler (0, 0, Random.Range (-45, 45)); //Establece la rotacion de manera aleatoria
+		comboText.fontSize = Random.Range (20, 30); //Establece el tamaño de la fuente de manera aleatoria
 	}
 }
 
