963481c2ba389e0d033391ea8d8bc900c03e53d6
Gustavo Garcia <GustavoGS_@outlook.com>
2016-01-31 15:03:43 -0600

    Revert "Revert "Added animations to main, game, state""
    This reverts commit 79f9647023d44fb9d3d0043a7f27fc736ea4732d.

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class timer: MonoBehaviour
+{
+	public int currentTime;	//Tiempo actual
+	public Text timeText;	//Campo de texto
+
+	void Start ()
+	{
+		countTime (); //Empieza a contar
+	}
+
+	void countTime ()
+	{
+		currentTime++; //Suma al tiempo 
+		timeText.text = currentTime.ToString (); //Muestra el texto
+		Invoke ("countTime", 1); //Continua contando
+	}
+}
