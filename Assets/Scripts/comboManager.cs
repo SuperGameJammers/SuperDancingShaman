7ee5b3ffd83f6166ee7624ee22614de6826345d6
Gustavo Garcia <GustavoGS_@outlook.com>
2016-01-31 14:42:31 -0600

    Added animations to main, game, state

--- Assets/Scripts/comboManager.cs
+++ Assets/Scripts/comboManager.cs
@@ -4,7 +4,8 @@ using UnityEngine.UI;
 
 public class comboManager: MonoBehaviour
 {
-	public Text comboText;	//Campo de texto
+	public Text[] comboText;				//Campo de texto
+	public Color defaultColor, wrongColor;	//Colores texto
 
 	void Update ()
 	{
@@ -17,26 +18,41 @@ public class comboManager: MonoBehaviour
 			if (SV.combo >= 3)
 			{
 				positionComboText (); //Posiciona el combo de manera aleatoria
-				comboText.text = SV.combo + " Combo!"; //Muestra el combo actual
+				foreach (var item in comboText)
+				{
+					item.color = defaultColor; //Colorea el texto
+					item.text = SV.combo + " Combo!"; //Muestra el combo actual
+				}
 			}
 			else
 			{
 				positionComboText (); //Posiciona el combo de manera aleatoria
-				comboText.text = "Well done"; //Vacia el campo de texto
+				foreach (var item in comboText)
+				{
+					item.color = defaultColor; //Colorea el texto
+					item.text = SV.combo + " Well done!"; //Muestra un mensaje
+				}
 			}
 		}
 		else if (Input.anyKeyDown)
 		{
 			SV.combo = 0; //Resetea el combo
 			positionComboText (); //Posiciona el combo de manera aleatoria
-			comboText.text = "Oops";
+			foreach (var item in comboText)
+			{
+				item.color = wrongColor; //Colorea el texto
+				item.text = " Oops!"; //Muestra un mensaje
+			}
 		}
 	}
 
 	void positionComboText ()
 	{
-		comboText.rectTransform.rotation = Quaternion.Euler (0, 0, Random.Range (-45, 45)); //Establece la rotacion de manera aleatoria
-		comboText.fontSize = Random.Range (20, 30); //Establece el tamaño de la fuente de manera aleatoria
+		foreach (var item in comboText)
+		{
+			item.rectTransform.rotation = Quaternion.Euler (0, 0, Random.Range (-45, 45)); //Establece la rotacion de manera aleatoria
+			item.fontSize = Random.Range (30, 35); //Establece el tamaño de la fuente de manera aleatoria
+		}
 	}
 }
 
